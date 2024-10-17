c811ee5db883546dea426ff70a74bd3d2758e37c
Alessandro Viana <alessandroviana830@gmail.com>
2024-10-17 18:35:11 -0300

    Publicando o projeto

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Spotify",
+  description: "Listen to music.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className="bg-zinc-900 text-zinc-50">{children}</body>
+    </html>
+  );
+}
